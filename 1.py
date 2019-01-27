e88b80a2147d17b39f024c4480c1f580cb5112e5
Inamdarpushkar <inamdarpushkar27@gmail.com>
2019-01-26 16:36:12 -0800

    l

---
+++ 1.py
@@ -0,0 +1,20 @@
+# Pyhton Object Oriented Programming
+
+class Employee:
+
+    def __init__(self,first,last,pay):
+        self.first=first
+        self.last=last
+        self.pay=pay
+        self.email=first+'.'+last+"@company.com"
+
+    def full_name(self):
+        return ('{} {}'.format(self.first,self.last))
+
+emp_1=Employee('Chinmay','Deshpande',50000)
+emp_2=Employee('Akshay','kulkarni',60000)
+
+#print(emp_1.email)
+#print(emp_2.email)
+
+print(emp_1.full_name())
